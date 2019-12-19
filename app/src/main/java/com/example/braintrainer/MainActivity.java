1859d93e38b8e72b08f3ea8f3e3c470e67eea966
salemalem <wymedia@protonmail.com>
2019-12-19 18:13:35 +0600

    v1.0 questions contain + operations only. User have scores and total number of answered questions. Timer doesn't work.

---
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button startButton;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+    int locationOfCorrectAnswer;
+    int score = 0;
+    int numberOfQuestions = 0;
+    TextView resultTextView;
+    TextView pointsTextView;
+    TextView sumTextView;
+    Button button0, button1, button2, button3;
+
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
+
+    public void generateQuestion() {
+        Random rand = new Random();
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+        int incorrectAnswer;
+        answers.clear();
+
+        for (int i = 0; i < 4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a + b);
+            } else {
+                incorrectAnswer = rand.nextInt(41);
+                while (incorrectAnswer == a + b) {
+                    incorrectAnswer = rand.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+    }
+
+    public void chooseAnswer(View view) {
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Log.i("correct", "correct");
+            score++;
+            resultTextView.setText("Correct!");
+        } else {
+            resultTextView.setText("Wrong!");
+        }
+//        Log.i("TAG:", view.getTag().toString());
+        numberOfQuestions++;
+        pointsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+
+        generateQuestion();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.startButton);
+        sumTextView = (TextView)findViewById(R.id.sumTextView);
+        pointsTextView = (TextView)findViewById(R.id.pointsTextView);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        button0 = (Button)findViewById(R.id.answer0);
+        button1 = (Button)findViewById(R.id.answer1);
+        button2 = (Button)findViewById(R.id.answer2);
+        button3 = (Button)findViewById(R.id.answer3);
+
+        generateQuestion();
+    }
+}
